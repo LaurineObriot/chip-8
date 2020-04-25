2099563ba3196b29abe987993c6710606cb7e431
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-04-25 21:51:48 +0200

    Add DW, modify formatter

--- classes/Disassembler.js
+++ classes/Disassembler.js
@@ -1,15 +1,10 @@
-const { INSTRUCTION_SET } = require('./constants/instructionSet')
+const { INSTRUCTION_SET } = require('../constants/instructionSet')
 
 class Disassembler {
   disassemble(opcode) {
     let instruction = INSTRUCTION_SET.filter(
       instruction => (opcode & instruction.mask) === instruction.pattern
     )[0]
-    console.log(instruction)
-
-    if (!instruction) {
-      throw new Error('Invalid instruction')
-    }
 
 	let args = instruction.arguments.map(arg => (opcode & arg.mask) >> arg.shift)
 
@@ -17,12 +12,26 @@ class Disassembler {
   }
 
   format(decodedInstruction) {
-    let formatted =
-      decodedInstruction.args.length > 0
-        ? `${decodedInstruction.instruction.name} ${decodedInstruction.args
-            .map(arg => `v${arg.toString(16)}`)
-            .join(', ')}`
-        : decodedInstruction.instruction.name
+		let types = decodedInstruction.instruction.arguments.map(arg => arg.type)
+		let args = decodedInstruction.args
+		let argsLookup = types.map((type, i) => [ type, args[i] ]) // zip it
+
+		argsLookup.forEach((type, i) => {
+		if (type[0] === 'K' || type[0] === 'KK' || type[0] === 'KKK') {
+		  argsLookup[i].push(`v${type[1].toString(16)}`)
+		} else {
+		  argsLookup[i].push(type[0] + type[1].toString(16))
+		}
+		})
+
+		let formatted
+
+		if (decodedInstruction.args.length > 0) {
+		formatted =
+		  decodedInstruction.instruction.name + ' ' + argsLookup.map(arg => arg[2]).join(', ')
+		} else {
+		formatted = decodedIntruction.instruction.name
+		}
     return formatted
   }
 
@@ -32,9 +41,9 @@ class Disassembler {
     for (let i = 0; i < data.length; i++) {
       let address = (i * 2).toString(16).padStart(6, '0')
       let opcode = data[i].toString(16).padStart(4, '0')
-      let operands = this.format(this.disassemble(data[i]))
+	  let operands = this.format(this.disassemble(data[i]))
 
-      lines.push(`${address} ${opcode} ${operands}`)
+	  lines.push(`${address}  ${opcode}  ${operands}`)
     }
 
     return lines.join('\n')
--- temp.js
+++ temp.js
@@ -1,4 +1,3 @@
-const { INSTRUCTION_SET } = require('./constants/instructionSet')
 let fs = require('fs')
 let filename = process.argv.slice(2)[0]
 let { RomBuffer } = require('./classes/RomBuffer')
@@ -6,15 +5,15 @@ let { Disassembler } = require('./classes/Disassembler')
 
 let romBuffer = new RomBuffer(filename, fs)
 
-console.log('RomBuffer - dump')
-console.log(romBuffer.dump())
+// console.log('RomBuffer - dump')
+// console.log(romBuffer.dump())
 
-let disassembler = new Disassembler(INSTRUCTION_SET)
-let decodedInstruction = disassembler.disassemble(0x5434)
+let disassembler = new Disassembler()
+// let decodedInstruction = disassembler.disassemble(0x8ab2)
 
-console.log('\nDisassembler - disassemble')
-console.log(decodedInstruction)
-console.log('\nDisassembler - format')
-console.log(disassembler.format(decodedInstruction))
-console.log('\nDisassembler - dump')
-console.log(disassembler.dump(romBuffer.data))
+// console.log('\nDisassembler - disassemble')
+// console.log(decodedInstruction)
+// console.log('\nDisassembler - format')
+// console.log(disassembler.format(decodedInstruction))
+// console.log('\nDisassembler - dump')
+// console.log(disassembler.dump(romBuffer.data))
