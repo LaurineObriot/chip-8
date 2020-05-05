4a544dbfe3217e63e38c4fd0e082c5bb2967ba13
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-05-05 10:35:07 +0200

    handle rom switch during wait key

--- classes/CPU.js
+++ classes/CPU.js
@@ -8,6 +8,8 @@ class CPU {
  	*/
 	constructor(cpuInterface) {
 	    this.interface = cpuInterface
+
+    	this.reset()
 	}
 
 	/**
@@ -34,7 +36,7 @@ class CPU {
         this.I = 0
         this.SP = -1
         this.PC = 0x200
-		this.halted = false
+		this.halted = true
 		this.soundEnabled = false
 		this.doThing = null
 		this.requestHalt = new Promise(resolve => {
@@ -55,6 +57,8 @@ class CPU {
 		// Get ROM data from ROM buffer
 		const romData = romBuffer.data
 		let memoryStart = 0x200
+
+		this.halted = false
 
 	    // Place ROM data in memory starting at 0x200
 		// Since memory is stored in an 8-bit array and opcodes are 16-bit, we have to store the opcodes
